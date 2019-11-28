c3384f54d6c4c211055f2e2a4494dea388a5cac8
amanikad <amaniibrahim@Amanis-MacBook-2.local>
2019-11-28 16:15:59 -0500

    change func

---
+++ email.php
@@ -0,0 +1,14 @@
+<?php
+	mail("yw1hbmk@gmail.com", "just a test", "test message", "no headers here");
+	$to = "yw1hbmk@gmail.com";
+	$from = $_POST['email'];
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$subject2 = "Copy of your email: " . $subject;
+	$message = $from . " / " . $name . " wrote the following:" . "\n\n" . $_POST['message'];
+	$message2 = "Thanks for getting in contact with me. I try to reply within a day or two. Here's a copy of your message. " . "\n\n" . $_POST['message'];
+	$headers = "From:" . $to;
+	$headers2 = "From:" . $to;
+	mail($to, $subject, $message, $headers);
+	mail($from, $subject2, $message2, $headers2);
+?>
